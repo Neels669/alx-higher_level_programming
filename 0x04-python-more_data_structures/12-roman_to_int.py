d980eb5f138d1134052ba544509eb6894371a6dd
Neelam <neelam_669@yahoo.com>
2023-08-16 06:21:34 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None or isinstance(roman_string, str) is False:
-        return None
+        return 0
     n = 0
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for i in roman_string:
@@ -10,6 +10,6 @@ def roman_to_int(roman_string):
     chars = ""
     for i in range(0, len(roman_string) - 1):
         chars = roman_string[i] + roman_string[i + 1]
-    if chars in subs:
-        n += subs[chars]
+        if chars in subs:
+            n += subs[chars]
     return n
