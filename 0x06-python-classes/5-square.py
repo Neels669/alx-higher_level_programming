df5bb6ca3d882c11afa4e72e5daef007812dbfea
Neelam <neelam_669@yahoo.com>
2023-08-29 06:29:45 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module defines a sqaure based on 4-square.py"""
+
+
+class Sqaure:
+    """This class represents a square attributed to size"""
+
+    def __init__(self, size=0):
+        """ initialize square objects,
+        sets size equal to 0 by default,
+        checks if size has the correct type and value """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
