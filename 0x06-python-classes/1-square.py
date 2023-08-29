a4199afadfcf6d22d5f7057cd9207db5bf4b8a3e
Neelam <neelam_669@yahoo.com>
2023-08-29 05:59:18 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a Square based on 0-square.py"""
+
+
+class Square:
+    """this class represents a Square"""
+    def __init__(self, size):
+        """Initialize the attribute size"""
+        self.__size = size
