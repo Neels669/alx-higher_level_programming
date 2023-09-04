d15430f5f4bd830908417f061e853a767b2bbade
Neelam <neelam_669@yahoo.com>
2023-09-04 05:05:52 -0700

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module defines a class Rectangle"""
+
+
+class Rectangle:
+    """This class defines a rectangle based on 0-rectangle.py"""
+
+    def __init__(self, width=0, height=0):
+        """ instantiation """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ getter for width """
+        return self.__width
+
+    @property
+    def height(self):
+        """ getter for height """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ setter for width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ setter for height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
