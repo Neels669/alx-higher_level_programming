9c5e867ae05521319a146b145f4af61cd3f3086b
Neelam <neelam_669@yahoo.com>
2023-09-11 11:55:16 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a clas BaseGeometry"""
+
+
+class BaseGeometry:
+    """based on 5-base_geometry.py"""
+
+    def area(self):
+        """raise exception is area is not implemented"""
+        raise Exception
