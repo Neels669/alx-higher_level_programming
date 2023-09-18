e38569448ac21268af117b3b948c648c76ed3e8f
Neelam <neelam_669@yahoo.com>
2023-09-18 13:29:11 -0700

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This class defines a Sqaure inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ the string representation of an object """
+        return "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.height)
