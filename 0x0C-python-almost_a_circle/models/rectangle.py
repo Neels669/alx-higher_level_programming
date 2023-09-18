95a72be488bbf394e5f1c2068ce838d20763e037
Neelam <neelam_669@yahoo.com>
2023-09-18 13:10:21 -0700

    Task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,8 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print('#', end="")
             print()
+
+    def __str__(self):
+        """ string representation of the object """
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
