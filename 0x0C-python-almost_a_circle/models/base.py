5e8e22b2cc2cf8cbd8d76f82204c2d6e31974e1c
Neelam <neelam_669@yahoo.com>
2023-09-21 06:13:46 -0700

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module defines class Base"""
+import json
+import csv
 
 
 class Base:
@@ -13,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
