b82f852a49924dbd2bcbfe583c01489b389eb141
Neelam <neelam_669@yahoo.com>
2023-11-17 05:16:54 -0800

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines to list states from a database"""
+import sys
+import MySQLdb
+
+
+def main(argv):
+    """connects to a given mysql database and lists all 'states' from it"""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        main(sys.argv)
