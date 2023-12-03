b5658c9dba8ea62c0a9c466517f42d9151e3078b
Neelam <neelam_669@yahoo.com>
2023-12-03 08:37:20 -0800

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": letter}
+    response = requests.post(url, data)
+    try:
+        json_response = response.json()
+        if json_response:
+            print(f"[{json_response['id']}] {json_response['name']}")
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
