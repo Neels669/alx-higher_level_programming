7e09520c00c97417720ad264cb762338f9319b31
Neelam <neelam_669@yahoo.com>
2023-12-03 07:31:00 -0800

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""POSTS an email using a given URL"""
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+
+print(body)
